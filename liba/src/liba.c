1646d9110c916aed9b264db3b0b58308bf932a17
Laurent Bouchet <lbouchet@irap.omp.eu>
2019-03-21 15:08:16 +0100

    OK

--- liba/src/liba.c
+++ liba/src/liba.c
@@ -6,4 +6,11 @@ void liba_hello() {
   fprintf(stdout, "%s\n", "hello from liba");
 }
 
+/* Add as required in https://doc.cc.in2p3.fr/developpements:formation:git */
+void liba_byebye() {
+  fprintf(stdout, "%s\n"," I just add this test function...");
+  fprintf(stdout, "%s\n"," I just add this test function...");
+}
+
+
 /* EOF */
